caf2031ccd29eda914a94c2c5de60b1bf6a86cbc
StephanieNigth <s.hvenegaard@outlook.com>
2017-09-25 12:14:59 +0200

    Fixed errors

--- 17_G12/src/game/MainGame.java
+++ 17_G12/src/game/MainGame.java
@@ -11,36 +11,14 @@ import gameframework.Game;
  *
  * @author Stephanie
  */
-public class MainGame {
+public class MainGame
+{
     // made by steph.
     public static void main(String[] args) 
-<<<<<<< HEAD
     {       
        Game g = new Game();
        g.play();
     }  
-=======
-    {
-        // this is the main guys
-       int i = 6;
-    }
-    
-    
-    // testing
-    private void notAThing() 
-    {
-        
-    }
-    
-    private void benjamin() 
-    {
-        
-    }   
+}
 
-private void malte() {
-for (int i = 0; i < 10; i++) {
-    System.out.println("Malte er awesome");
-}
-}
->>>>>>> 0ffd6ad58296b432fbf2b1c179ad89358cc0e0e9
-}
+
