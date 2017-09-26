708ca5058484882ae950e1deb8be8a9d243e70cc
Maltesj <malte_sj@hotmail.com>
2017-09-26 19:41:11 +0200

    Made Character class ready for Combat class

--- 17_G12/src/gameframework/Character.java
+++ 17_G12/src/gameframework/Character.java
@@ -11,16 +11,34 @@ package gameframework;
  */
 public class Character 
 {
-    Character(String characterName, String description, int healthPoints)
+    private String characterName = "";
+    private String description = "";
+    private int healthPoints = 100;
+    private int attackDamage = 0;
+    private int attackHitModifier = 0;
+    private int defenseEvation = 0;
+    private int defenseArmor = 0;
+    private int level =1;
+    // private Item weapon;
+    // private Item armor;
+    // private Item shield;
+    
+    Character(String characterName, String description, int healthPoints, 
+            int attackDamage, int attackHitModifier, int defenseArmor, 
+            int defenseEvation, int level)
     {   
         this.characterName = characterName;
         this.description = description;
-        this.healthPoints = healthPoints;                
+        this.healthPoints = healthPoints; 
+        this.attackDamage = attackDamage;
+        this.attackHitModifier = attackHitModifier;
+        this.defenseArmor = defenseArmor;
+        this.defenseEvation = defenseEvation;
+        this.level = level;
     }
     
-    private String characterName;
-    private String description;
-    private int healthPoints;
+    
+    
     //private imageFile image;
     /**
      * Setter for characterName
@@ -78,7 +96,44 @@ public class Character
     {
         return this.healthPoints;
     }
+    /**
+     * 
+     * @param character
+     * @return 
+     */
+    public int getAttackDamage(Character character)
+    {
+        return this.attackDamage;
+    }
+    public int getAttackHitModifier(Character character)
+    {
+        return this.attackHitModifier;
+    }
+    public int getDefenseEvation(Character character) 
+    {
+        return this.defenseEvation;
+    }
+    public int getDefenseArmor(Character character)
+    {
+        return this.defenseArmor;
+    }
     
+    public void setAttackDamage(int attackDamage)
+    {
+        this.attackDamage = attackDamage;
+    }
+    public void setAttackHitModifier(int attackHitModifier)
+    {
+        this.attackHitModifier = attackHitModifier;
+    }
+    public void setDefenseEvation(int defenseEvation)
+    {
+        this.defenseEvation = defenseEvation;
+    }
+    public void setDefenseArmor(int defenseArmor)
+    {
+        this.defenseArmor = defenseArmor;
+    }
     /*
     soudo kode fordi jeg ikke helt ved hvordan det skal gøres
     
