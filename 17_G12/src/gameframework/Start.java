a9167a130c40d14b1559a812247ddf99b60395b7
StephanieNigth <s.hvenegaard@outlook.com>
2017-10-03 12:13:32 +0200

    fix 01
    DEL 1 AF MANGE TIL FIX

--- 17_G12/src/gameframework/Start.java
+++ 17_G12/src/gameframework/Start.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,30 +10,14 @@ package gameframework;
  * @author Stephanie
  */
 public class Start {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        /*
         Game g = new Game();
         long time = System.currentTimeMillis();
         g.playTEST();
         time = System.currentTimeMillis()-time;
         System.out.println("Execution in ms: " +time);
+*/
     }
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package gameframework;
-
-/**
- *
- * @author Stephanie
- */
-public class Start {
-    public static void main(String[] args) {
-        Game g = new Game();
-        g.playTEST();
-    }
-}
->>>>>>> aa70eead8673139b2dbc53771c027b24985376c8
