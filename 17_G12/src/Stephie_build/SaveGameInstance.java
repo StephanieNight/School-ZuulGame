3151b74efd16f6fedd7641341baf52d62a6c2e1e
StephanieNigth <s.hvenegaard@outlook.com>
2017-11-10 10:44:18 +0100

    Chanced acces
    Dir is now public and a save game instance hans been created

---
+++ 17_G12/src/Stephie_build/SaveGameInstance.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Stephie_build;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Stephanie
+ */
+public class SaveGameInstance implements Serializable {
+    
+    
+}
