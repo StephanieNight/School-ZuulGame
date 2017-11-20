58e367d361777fa1bc6b40f8b96c2c1263999efe
minner82 <minner82.jj@gmail.com>
2017-11-20 11:34:21 +0100

    MovePlayer and View moved to legacy.

--- 17_G12/src/Legacy_framework/View.java
+++ 17_G12/src/Legacy_framework/View.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Jacobs_package;
+package Legacy_framework;
 
 import core_engine.Labyrinth;
 import core_engine.Room;
