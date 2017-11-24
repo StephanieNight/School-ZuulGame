31f3907c88ac14e978e6a3af4c7715516fcd3207
StephanieNigth <s.hvenegaard@outlook.com>
2017-11-24 15:32:22 +0100

    fix

--- 17_G12/src/core_engine/ZuulGame.java
+++ 17_G12/src/core_engine/ZuulGame.java
@@ -472,7 +472,7 @@ public class ZuulGame implements IGameConstants {
     }
     public void pickUpItem(int itemNumber) {
         if(player.getCurrentRoom().itemList().length > 1)
-        player.getCurrentRoom().useItem(itemNumber, player);
+        player.getCurrentRoom().pickupItem(itemNumber,player.getInventory());
     }
     public String getLootItemDescription(int itemNumber) {
         if(player.getCurrentRoom().itemList().length > 1)
