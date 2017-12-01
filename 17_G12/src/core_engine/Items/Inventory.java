61cd3ed8e836ae12397ddcdd18bf8b471b3da2e3
minner82 <minner82.jj@gmail.com>
2017-12-01 14:10:26 +0100

    Cleaning on Inventory
    Fixed and added comments to inventory.
    Also made minor changes to functionality and removed unnecessary if conditions.

--- 17_G12/src/core_engine/Items/Inventory.java
+++ 17_G12/src/core_engine/Items/Inventory.java
@@ -7,6 +7,7 @@ package core_engine.Items;
 import core_engine.Message;
 import core_engine.Player;
 import java.io.Serializable;
+
 /**
  * inventory to hold items
  * inventory length is determined by difficulty.
@@ -15,11 +16,9 @@ import java.io.Serializable;
  */
 
 
-
-// skal måske laves til set i stedet for array
 public class Inventory implements Serializable{
     private final Item[] inventory;
-    private Message msg;
+    private final Message msg;
     private Player player;
     private final int diff;
     
@@ -34,15 +33,19 @@ public class Inventory implements Serializable{
         player = p;
     }
     
-    public String[] getInventoryList() //shows player's inventory
+    /**
+     * 
+     * @return the String[] of the player's item's names.
+     */
+    public String[] getInventoryList()
     {
         int i = 0;
-        String[] invString = new String[8-diff];
+        String[] invString = new String[8-diff];//length of string is the same as length of inventory.
         for(Item list: inventory){
-            if(list == null){
+            if(list == null){//stops when inventory is out of items to look at.
                 break;
             }
-            invString[i] = list.getName();
+            invString[i] = list.getName();//places the names of inventory into the String[].
             i++;
         }
         return invString;
@@ -50,17 +53,17 @@ public class Inventory implements Serializable{
     
     
     /**
-     * loops through items in inventory to find an item with a matching name.
-     * @param itemID
+     * activates the effect of a selected item.
+     * @param itemID determines which item to use.
      * executes useItem if names matches and removes said item and shifting the 
-     * rest of the items to the left.
-     * @return 
-     * returns true if an item was used, false if item wasn't used.
+     * rest of the items back a slot.
+     * @return true if the used item was consumable, false if item wasn't.
      */
     public boolean useItem(int itemID)
     {
-        if(itemID > inventory.length - 1 || inventory[itemID] == null)
+        if(inventory[itemID] == null)
         {
+            msg.setDescription("No item selected.");
             return false;
         }
         
@@ -68,35 +71,35 @@ public class Inventory implements Serializable{
 
         if (!"Consumable".equalsIgnoreCase(inventory[itemID].getType())) 
             {
-                return false; //if the used item is equipment, item isn't used, and returns false.
+                return false; //if the used item not consumable, item isn't used, and returns false.
             }
 
         for(;itemID < inventory.length-1; itemID++) //cycles through remainder of inventory.
         {
             inventory[itemID] = inventory[itemID+1]; //destroys used item and moves all items one index down.
         }
-        inventory[inventory.length-1] = null; //removes last item in inventory to avoid duplicating it.7
+        inventory[inventory.length-1] = null; //removes last item in inventory to avoid duplicating it.
         return true;
     }
     
     /**
-     * prints the name and description of item matching input ID.
+     * Gets the description of item matching input ID.
      * @param itemID 
-     * @return 
+     * @return The description of item matching input ID.
      */
-    
     public String getItemDescription(int itemID)
     {
-        if(itemID < inventory.length && inventory[itemID] != null){
+        if(inventory[itemID] != null)
+        {
             return inventory[itemID].getDescription();
         }
         return "No item selected.";
     }
     
     /**
-     * checks if inventory has any available space and if item type is already 
-     * on the list.
-     * @param item 
+     * checks if inventory has any available space and if equipment type 
+     * is already on the list.
+     * @param item the item that wants to be added.
      * @return returns true if item was succesfully added, otherwise false.
      */
     public boolean addItem(Item item)
@@ -113,7 +116,6 @@ public class Inventory implements Serializable{
                         msg.setDescription("Picked up " + item.getName() + " and dropped your " + inventory[i].getName());
                         player.getCurrentRoom().dropItem(inventory[i]); //drops current equipment
                         inventory[i] = item; //replaces equipment with new.
-                        //remove item from room
                         updateStat();
                         return true;
                     }
@@ -121,9 +123,9 @@ public class Inventory implements Serializable{
                 else//if you're not already wearing same type of equipment and inventory is not full, pick it up.
                 {
                     msg.setDescription(item.getName() + " was picked up.");
-                        inventory[i] = item; //adds item to first vacant spot in inventory.
-                        updateStat();
-                        return true;
+                    inventory[i] = item; //adds item to first vacant spot in inventory.
+                    updateStat();
+                    return true;
                 }
             }
         }
@@ -143,52 +145,50 @@ public class Inventory implements Serializable{
    
     
     /**
-     * Lets the player delete items.
+     * Lets the player delete items from inventory.
      * @param itemID is a String matching a name of an item in the inventory array
-     * You cannot drop your key as it is needed to win.
+     * 
      */
     public void dropItem(int itemID)
     {
-        if(itemID < inventory.length && inventory[itemID] != null){
+        if(inventory[itemID] != null){
             msg.setDescription("You dropped your " + inventory[itemID].getName());
             player.getCurrentRoom().dropItem(inventory[itemID]);
             inventory[itemID] = null;
 
-            for(;itemID < inventory.length-1; itemID++) //all items following used item are put one index lower to fill the hole.
-            {
+            for(;itemID < inventory.length-1; itemID++) 
+            {//all items following used item are put one index lower to fill the hole.
                 inventory[itemID] = inventory[itemID+1];
             }
             inventory[inventory.length-1] = null; //the last item is (re)moved.
             updateStat();
         }
+        else{
+            msg.setDescription("No item selected.");
+        }
     }
     
     
     /**
-     * updates the stats the player gets from items.
+     * updates the stats the player gets from equipment.
      */
     public void updateStat()
     {
-        player.setWeapon(0);
+        player.setWeapon(0);//sets all equipment values to 0 in case no equipment of appropriate type is worn.
         player.setArmor(0);
         player.setShield(0);
-        for (int i = 0; i < inventory.length; i++)
-        {
-            if(inventory[i] == null)
-            {
+        for (Item inventory1 : inventory) {
+            if (inventory1 == null) {
                 break;
+            }//sets the stat for each appropriate equipment type(if in inventory).
+            if ("Weapon".equals(inventory1.getType())) {
+                player.setWeapon(inventory1.getStat());
             }
-            if("Weapon".equals(inventory[i].getType()))
-            {
-                player.setWeapon(inventory[i].getStat());
+            if ("Armor".equals(inventory1.getType())) {
+                player.setArmor(inventory1.getStat());
             }
-            if("Armor".equals(inventory[i].getType()))
-            {
-                player.setArmor(inventory[i].getStat());
-            }
-            if("Shield".equals(inventory[i].getType()))
-            {
-                player.setShield(inventory[i].getStat());
+            if ("Shield".equals(inventory1.getType())) {
+                player.setShield(inventory1.getStat());
             }
         }
     }
