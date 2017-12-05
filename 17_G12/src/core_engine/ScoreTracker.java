dceaf3cac3a763b3b43d79eb258f6c21209c988d
StephanieNigth <s.hvenegaard@outlook.com>
2017-12-05 15:18:59 +0100

    stuff

--- 17_G12/src/core_engine/ScoreTracker.java
+++ 17_G12/src/core_engine/ScoreTracker.java
@@ -5,33 +5,18 @@
  */
 package core_engine;
 
-import core_engine.GameEngine;
 import acquaintance.IGameConstants;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import core_engine.Player;
 
 /**
  * Class for tracking the score for the current game
  * @author Malte
  */
-public class ScoreTracker implements Serializable {
-    
+public class ScoreTracker implements Serializable {    
     private Score score;
     private int currentScore;
     private Player player;
-    private int diff;
-
-    
+    private int diff;    
     /**
      * set up the score tracker with the player and the difficulty
      * @param p player to be traced
