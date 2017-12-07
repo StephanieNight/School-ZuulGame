961fc434959e053d7958413e16146c9b5936e746
Maltesj <malte_sj@hotmail.com>
2017-12-07 12:21:53 +0100

    Comments to player and monster

--- 17_G12/src/core_engine/Monster.java
+++ 17_G12/src/core_engine/Monster.java
@@ -4,12 +4,28 @@ package core_engine;
 import core_engine.GameEngine;
 
 /**
- *
+ * the Monster class extending Actor, used for monsters with the only specific 
+ * method used to distinguish between normal monsters and the boss monster
  * @author nicol
  */
 public class Monster extends Actor
 {
     private final boolean IS_BOSS;
+    
+    /**
+     * The constructor for the monster class, updating the values of the monster 
+     * to match the level they are set to
+     * @param name the name of the monster
+  @param defaultHealthpoint starting health points before adding extra from levels
+     * @param defaultDefense starting defense before adding extra from levels
+     * @param DefaultDamgeOutput starting damage done before adding extra from levels
+     * @param mapCode the char used to show the monster, is used in some methods 
+     * to distinguish between normal and boss monsters
+     * @param level the level the monsters will be.
+     * @param isBoss a boolean, true if the monster is a boss
+     * @param difficulty the current difficulty
+     * @param msg the message class
+     */
     
     public Monster(String name, int defaultHealthpoint, int defaultDefense, int defaultDamgeOutput, char mapCode, int level, boolean isBoss, int difficulty, Message msg) {
         super(name, defaultHealthpoint, defaultDefense, defaultDamgeOutput, mapCode, level, difficulty, msg);
@@ -17,6 +33,11 @@ public class Monster extends Actor
         updateLevel();
         setCurrentHealth(10000);
     }    
+    
+    /**
+     * the method to access the 
+     * @return boolean true if boss, false if normal monster
+     */
     public boolean isBoss() {
         return IS_BOSS;
     }
--- 17_G12/src/core_engine/Player.java
+++ 17_G12/src/core_engine/Player.java
@@ -12,7 +12,7 @@ import core_engine.Items.Inventory;
 import core_engine.Items.Item;
 
 /**
- *
+ * the Player class extending Actor, used for the player to hold items and player specific methods
  * @author nicol
  */
 public class Player extends Actor{
@@ -20,7 +20,17 @@ public class Player extends Actor{
      private int lampOil;
      private Inventory inventory;
     
-    
+    /**
+     * the constructor for the Player class, initiates an inventory for the player, 
+     * adding starting items and sets the time available by adding lampOil.
+     * @param name the player name
+     * @param defaultHealthpoint starting health points before adding extra from levels
+     * @param defaultDefense starting defense before adding extra from levels and armor / shield
+     * @param DefaultDamgeOutput starting damage done before adding extra from levels and weapon
+     * @param level starts at 1
+     * @param difficulty the current difficulty, needed for the inventory.
+     * @param msg the message class shared by all classes.
+     */
     public Player(String name, int defaultHealthpoint, int defaultDefense, int DefaultDamgeOutput, int level, int difficulty, Message msg) {
         super(name, defaultHealthpoint, defaultDefense, DefaultDamgeOutput, 'P', level, difficulty, msg);
         this.inventory = new Inventory(difficulty, msg);
@@ -31,20 +41,40 @@ public class Player extends Actor{
         lampOil = 25 + difficulty * 25;
         inventory.updateStat();
     }  
+    
+    /**
+     * the time available measured in oil for a lamp.
+     * @return integer value of lampOil
+     */
     public int getLampOil(){
         return lampOil;
     }
+    /**
+     * adds 20 to current lampOil when called.
+     */
     public void addLampOil(){
         lampOil +=20;
    }
+    /**
+     * subtracts 1 from current lampOil when called.
+     */
     public void useLampOil(){
         lampOil -= 1;
     }
+    /**
+     * used to access the players inventory.
+     * @return inventory
+     */
     public Inventory getInventory()
     {
         return inventory;
     }
     
+    /**
+     * an easy access to the method in inventory class
+     * @param item the item that should be added to the inventory ArrayList
+     * @return true if the item is successfully added
+     */
     public boolean addItem(Item item)
     {
         return inventory.addItem(item);
