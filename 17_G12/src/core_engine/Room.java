65dcdf2cd37f4abe6042284340b350a6c7a851f8
Maltesj <malte_sj@hotmail.com>
2017-12-11 12:00:52 +0100

    ghoust -> ghost

--- 17_G12/src/core_engine/Room.java
+++ 17_G12/src/core_engine/Room.java
@@ -9,274 +9,304 @@ import core_engine.Items.Item;
 import java.io.Serializable;
 
 /**
- * @author  Michael Kolling and David J. Barnes
+ * @author Michael Kolling and David J. Barnes
  * @version 2006.03.30
  */
-public class Room implements Serializable
-{
+public class Room implements Serializable {
+
     private String description;
     private HashMap<String, Room> exits;
     private HashMap<String, Door> doors;
     private Actor player; // Copy
     private Actor monster; // Copy
-    private Actor ghoust; // Copy
+    private Actor ghost; // Copy
     private ArrayList<Item> lootList;
     private int pictureID_3D;
     private int pictureID_MiniMap;
     private boolean playerVissited;
     private boolean isExit; // one room is the exit of the maze
+
     /**
-     * basic constructor for a room 
-     * set the description, if the player has visited to false because no the 
-     * player has not yet, and sets the Hashmaps of doors and exits.
+     * basic constructor for a room set the description, if the player has
+     * visited to false because no the player has not yet, and sets the Hashmaps
+     * of doors and exits.
+     *
      * @param description the rooms decription
      */
-    public Room(String description) 
-    {
+    public Room(String description) {
         this.description = description;
         exits = new HashMap<>();
         doors = new HashMap<>();
         lootList = new ArrayList<>();
-        this.pictureID_3D=0;
-        this.pictureID_MiniMap =0;
+        this.pictureID_3D = 0;
+        this.pictureID_MiniMap = 0;
         this.playerVissited = false;
     }
+
     /**
-     * set the description, if the player has visited to false because no the 
-     * player has not yet, and sets the Hashmaps of doors and exits. 
-     * 
+     * set the description, if the player has visited to false because no the
+     * player has not yet, and sets the Hashmaps of doors and exits.
+     *
      * takes an array of items to add to its loot list
+     *
      * @param description the rooms decription
      * @param items the array of loot to add
      */
-    public Room(String description, Item[] items) 
-    {
+    public Room(String description, Item[] items) {
         this(description);
         Collections.addAll(lootList, items);
     }
+
     /**
-     * set the description, if the player has visited to false because no the 
-     * player has not yet, and sets the Hashmaps of doors and exits and adds 
-     * a single item to the loot list.
+     * set the description, if the player has visited to false because no the
+     * player has not yet, and sets the Hashmaps of doors and exits and adds a
+     * single item to the loot list.
+     *
      * @param description the rooms decription
      * @param item item to add to loot list
      */
-    public Room(String description, Item item)
-    {
+    public Room(String description, Item item) {
         this(description);
         lootList.add(item);
     }
+
     /**
      * set if the room is final exit.
-     * @param isExit true ? 
+     *
+     * @param isExit true ?
      */
     public void setIsExit(boolean isExit) {
         this.isExit = isExit;
     }
+
     /**
      * get if the room is Exit
+     *
      * @return true if the room is exit if not false.
      */
     public boolean isExit() {
         return isExit;
-    }    
+    }
+
     /**
-     * 
-     * @param pictureID_3D 
+     *
+     * @param pictureID_3D
      */
     public void setPictureID_3D(int pictureID_3D) {
         this.pictureID_3D = pictureID_3D;
     }
+
     public void setPictureID_MiniMap(int pictureID_MiniMap) {
         this.pictureID_MiniMap = pictureID_MiniMap;
     }
+
     public int getPictureID_3D() {
         return pictureID_3D;
     }
+
     public int getPictureID_MiniMap() {
         return pictureID_MiniMap;
     }
+
     public boolean isPlayerVissited() {
         return playerVissited;
     }
-    
-    
-    public void setExit(String direction, Room neighbor) 
-    {
+
+    public void setExit(String direction, Room neighbor) {
         exits.put(direction, neighbor);
     }
+
     /**
-     * 
+     *
      * Gets the description of the Room
+     *
      * @return Description
      */
-    public String getShortDescription()
-    {
+    public String getShortDescription() {
         return description;
     }
+
     /**
      * Gets a formated string with the description of the room and it's exists
+     *
      * @return Long Decription of the room
      */
-    public String getLongDescription()
-    {
+    public String getLongDescription() {
         return "You are " + description + ".\n" + getExitString();
     }
+
     /**
      * a string with all the exits of the room
-     * @return Exists  
+     *
+     * @return Exists
      */
-    private String getExitString()
-    {
+    private String getExitString() {
         String returnString = "Exits:";
         Set<String> keys = exits.keySet();
-        for(String exit : keys) {
+        for (String exit : keys) {
             returnString += " " + exit;
         }
         return returnString;
     }
+
     /**
-     * 
+     *
      * @param direction you want to go in, North, South, West and east
      * @return Room at the Exit
      */
-    public Room getExit(String direction) 
-    {
+    public Room getExit(String direction) {
         return exits.get(direction);
     }
-    public void setPlayer(Actor actor)
-    {
-        player = actor;  
+
+    public void setPlayer(Actor actor) {
+        player = actor;
         playerVissited = true;
     }
-    public void setMonster(Actor actor)
-    {
-        monster = actor;         
+
+    public void setMonster(Actor actor) {
+        monster = actor;
     }
-    public Actor getPlayer()
-    {
+
+    public Actor getPlayer() {
         return player;
     }
-    public Actor getMonster()
-    {
-        
+
+    public Actor getMonster() {
+
         return monster;
     }
-    public boolean isConflict()
-    {
-        return(player !=null && monster != null);
+
+    public boolean isConflict() {
+        return (player != null && monster != null);
     }
-    public boolean hasExit(String direction)
-    {
+
+    public boolean hasExit(String direction) {
         return exits.containsKey(direction);
     }
-    public boolean hasAnyExits()
-    {
+
+    public boolean hasAnyExits() {
         return !exits.isEmpty();
     }
+
     public char getMonsterMapCode() {
-        if(isConflict())
-        {
+        if (isConflict()) {
             return 'C';
-        }
-       else if (this.player !=null)
+        } else if (this.player != null) {
             return player.getMapCode();
-       else if(this.monster != null)
-           return monster.getMapCode();
-       else 
-        return ' ';
+        } else if (this.monster != null) {
+            return monster.getMapCode();
+        } else {
+            return ' ';
+        }
     }
-    /** 
-     * gets the exits 
+
+    /**
+     * gets the exits
+     *
      * @return stirng array of exit directions.
      */
     public String[] getExits() {
         Set<String> keys = exits.keySet();
-        int i =0;
+        int i = 0;
         String[] e = new String[keys.size()];
-        for(String s : keys)
-        {
+        for (String s : keys) {
             e[i] = s;
             i++;
         }
-        return e;    
+        return e;
     }
+
     /**
      * Changes the rooms description
+     *
      * @param desc new string description.
      */
     public void changeDescription(String desc) {
         this.description = desc;
     }
+
     /**
      * does the room has any items
+     *
      * @return true id lootlist size is greater then 0 - false if not.
      */
-    public boolean hasItems(){
+    public boolean hasItems() {
         return (lootList.size() > 0);
-    }    
+    }
+
     /**
-     * get the loot list 
+     * get the loot list
+     *
      * @return array of loot in the room.
      */
     public Item[] itemList() {
         Item itemList[] = new Item[lootList.size()];
         return lootList.toArray(itemList);
-    }    
+    }
+
     /**
      * pick up and item and places it in an inventory
+     *
      * @param id id of item
      * @param inventory inventory that the item is added to.
      */
     public void pickupItem(int id, Inventory inventory) {
         Item returnItem = lootList.get(id);
-        if(inventory.addItem(returnItem)){
+        if (inventory.addItem(returnItem)) {
             lootList.remove(id);
         }
-    }    
+    }
+
     /**
      * adds item to the loot list
-     * @param item 
+     *
+     * @param item
      */
     public void dropItem(Item item) {
         lootList.add(item);
     }
+
     /**
-     * adds a door in direction 
+     * adds a door in direction
+     *
      * @param direction direction that the door is in
      */
     public void setDoor(String direction) {
-        doors.put(direction,new Door(true));
+        doors.put(direction, new Door(true));
     }
-    public boolean hasDoor(String direction) {        
+
+    public boolean hasDoor(String direction) {
         return doors.containsKey(direction);
     }
+
     public Door getDoor(String direction) {
-        if(hasDoor(direction))
-        {
+        if (hasDoor(direction)) {
             return doors.get(direction);
-            
+
         }
-        return null;        
-    }    
+        return null;
+    }
+
     public void setPlayerVisisted() {
         playerVissited = true;
     }
+
     public void useItem(int id, Player p) {
-        if(itemList()[id].useItem(p))
-        {
+        if (itemList()[id].useItem(p)) {
             lootList.remove(id);
         }
     }
+
     public Actor getGhoust() {
-        return ghoust;
+        return ghost;
     }
+
     public void setGhoust(Actor gaust) {
-        this.ghoust = gaust;
+        this.ghost = gaust;
     }
+
     public boolean isOccupied() {
-        boolean o = (ghoust !=null || monster != null);                
+        boolean o = (ghost != null || monster != null);
         return o;
     }
 }
-            
